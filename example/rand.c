f0ed7990b98b26a9506fe20838c78e4e57c81c94
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2011-06-16 10:50:10 +0900

    add a parameter option to example/rand for the prng initialization

--- example/rand.c
+++ example/rand.c
@@ -2,7 +2,7 @@
  * This file shows how to use mt.c. It is released in the public
  * domain and as such comes with no copyright requirement.
  *
- * compile with: cc example.c mt.c
+ * compile with: cc rand.c mt.c
  */
 
 #include <stdlib.h>
@@ -12,20 +12,25 @@
 /* include the mt prototypes */
 #include "mt.h"
 
-int main()
+int main(int argc, char *const *argv)
 {
     int i;
 
-    /*
-     * a possible (weak) initialization
-     * uses the current time, in seconds
-     */
-    mt_init((unsigned long int) time(NULL));
+    if (1 < argc)
+        /* if a parameter is given, it is used for the initialisation */
+        mt_init((unsigned long int) atol(argv[1]));
+    else {
+        /*
+         * without parameter, the (weak) initialization
+         * uses the current time, in seconds
+         */
+        mt_init((unsigned long int) time(NULL));
+    }
 
     /*
-     * now we can get 1000 pseudo-random value
+     * now we can get 1024 pseudo-random value
      */
-    for (i = 0; i < 1000; i++)
+    for (i = 0; i < 1024; i++)
         printf("%10.8f\n", mt_drand53());
 
     return EXIT_SUCCESS;
